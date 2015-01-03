5e50c2633e989e9f0eabbdec076c81b2bb0d496d
allknowingfrog <allknowingfrog@gmail.com>
2015-01-02 21:19:55 -0600

    add border collision and basic collision function, pass canvas and player size from server, add jumping

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var users = {};
 var players = {};
+
+var CANVAS_W = 600;
+var CANVAS_H = 600;
+var PLAYER_W = 50;
+var PLAYER_H = 50;
+
 var FPS = 60;
 var SPEED = 1;
+var JUMP = 40;
 
 server.listen(3000);
 
@@ -18,16 +25,23 @@ io.sockets.on('connection', function(socket){
         if(data in users) {
             callback(false);
         } else {
-            callback(true);
+            callback({
+                cw: CANVAS_W,
+                ch: CANVAS_H,
+                pw: PLAYER_W,
+                ph: PLAYER_H
+            });
             socket.nickname = data;
             users[socket.nickname] = socket;
             players[socket.nickname] = {
                 x: 0,
-                y: 0,
+                y: (CANVAS_H - PLAYER_H),
                 up: false,
                 down: false,
                 left: false,
-                right: false
+                right: false,
+                jump: false,
+                speed: 0
             };
             io.sockets.emit('usernames', Object.keys(users));
         }
@@ -78,11 +92,16 @@ setInterval(gameLoop, 1000/FPS);
 
 function gameLoop() {
     for(var p in players) {
-        if(players[p].up) {
+        if(players[p].jump) {
+            players[p].speed -= 1;
+            players[p].y -= Math.floor(players[p].speed/10) + 1;
+        } else if(players[p].up) {
+            players[p].jump = true;
+            players[p].speed = JUMP;
             players[p].y -= SPEED;
         }
         if(players[p].down) {
-            players[p].y += SPEED;
+            players[p].jump = false;
         }
         if(players[p].left) {
             players[p].x -= SPEED;
@@ -90,6 +109,33 @@ function gameLoop() {
         if(players[p].right) {
             players[p].x += SPEED;
         }
+
+        if(players[p].x < 0) players[p].x = 0;
+        if(players[p].x > CANVAS_W - PLAYER_W) players[p].x = CANVAS_W - PLAYER_W;
+        if(players[p].y < 0) players[p].y = 0;
+        if(players[p].y > CANVAS_H - PLAYER_H) {
+            players[p].y = CANVAS_H - PLAYER_H;
+            players[p].jump = false;
+            players[p].speed = 0;
+        }
     }
     io.sockets.emit('update', players);
+}
+
+function collision(x,y,w,h,xx,yy,ww,hh) {
+    var x2 = x + w;
+    var y2 = y + h;
+    var xx2 = xx + ww;
+    var yy2 = yy + hh;
+    if (x > xx && x < xx2 && y > yy && y < yy2) {
+        return true;
+    } else if (x2 > xx && x2 < xx2 && y > yy && y < yy2) {
+        return true;
+    } else if (x > xx && x < xx2 && y2 > yy && y2 < yy2) {
+        return true;
+    } else if (x2 > xx && x2 < xx2 && y2 > yy && y2 < yy2) {
+        return true;
+    } else {
+        return false;
+    }
 }
