177f9b675c9b40f5b6a9523ee88a74924d7bd0a7
allknowingfrog <allknowingfrog@gmail.com>
2015-01-04 15:28:44 -0600

    create player module to handle all player functionality

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+var player = require('./player.js');
+
 var users = {};
 var players = {};
 var platforms = [];
 
-var CANVAS_W = 600;
-var CANVAS_H = 600;
-var PLAYER_W = 50;
-var PLAYER_H = 50;
-
-var FPS = 60;
-var SPEED = 1;
-var JUMP = 40;
-var JUMP_DIV = 10;
+//globals
+CANVAS_W = 600;
+CANVAS_H = 600;
+PLAYER_W = 50;
+PLAYER_H = 50;
+JUMP = 40;
+SPEED = 1;
+ACCEL_TICKS = 10;
+FPS = 60;
 
 server.listen(3000);
 
@@ -28,58 +31,21 @@ io.sockets.on('connection', function(socket){
             callback(false);
         } else {
             callback({
-                cw: CANVAS_W,
-                ch: CANVAS_H,
-                pw: PLAYER_W,
-                ph: PLAYER_H,
-                pf: platforms
+                CANVAS_W: CANVAS_W,
+                CANVAS_H: CANVAS_H,
+                platforms: platforms
             });
             socket.nickname = data;
             users[socket.nickname] = socket;
-            players[socket.nickname] = {
-                x: 0,
-                y: (CANVAS_H - PLAYER_H),
-                up: false,
-                down: false,
-                left: false,
-                right: false,
-                fall: false,
-                speed: 0
-            };
+            players[socket.nickname] = new player(socket.nickname, 0, 0, PLAYER_W, PLAYER_H);
             io.sockets.emit('usernames', Object.keys(users));
         }
     });
     socket.on('keyDown', function(data) {
-        switch(data) {
-            case 'up':
-                players[socket.nickname].up = true;
-                break;
-            case 'down':
-                players[socket.nickname].down = true;
-                break;
-            case 'left':
-                players[socket.nickname].left = true;
-                break;
-            case 'right':
-                players[socket.nickname].right = true;
-                break;
-        }
+        players[socket.nickname].keyDown(data);
     });
     socket.on('keyUp', function(data) {
-        switch(data) {
-            case 'up':
-                players[socket.nickname].up = false;
-                break;
-            case 'down':
-                players[socket.nickname].down = false;
-                break;
-            case 'left':
-                players[socket.nickname].left = false;
-                break;
-            case 'right':
-                players[socket.nickname].right = false;
-                break;
-        }
+        players[socket.nickname].keyUp(data);
     });
     socket.on('disconnect', function(data) {
         if(socket.nickname) {
@@ -91,151 +57,43 @@ io.sockets.on('connection', function(socket){
     });
 });
 
-addPlatform(PLAYER_W, CANVAS_H - PLAYER_H, PLAYER_W, PLAYER_H);
-addPlatform(PLAYER_W*3, CANVAS_H - PLAYER_H*2, PLAYER_W, PLAYER_H);
+platforms[platforms.length] = {x: PLAYER_W, y: CANVAS_H - PLAYER_H, w: PLAYER_W, h: PLAYER_H};
+platforms[platforms.length] = {x: PLAYER_W*3, y: CANVAS_H - PLAYER_H*2, w: PLAYER_W, h: PLAYER_H};
 
 setInterval(gameLoop, 1000/FPS);
 
 function gameLoop() {
-    movePlayers();
-    playerCollision();
-    io.sockets.emit('update', players);
-}
-
-function movePlayers() {
-    var player, plat;
-    for(var p in players) {
-        player = players[p];
-        if(player.fall) {
-            player.speed += 1;
-            player.y += Math.ceil(player.speed/JUMP_DIV);
-            plat = platCollision(player);
-            if(plat) {
-                if(player.speed <= 0) {
-                    player.y = plat.y + plat.h;
-                    player.speed = 1;
-                } else {
-                    player.y = plat.y - PLAYER_H;
-                    player.fall = false;
-                    player.speed = 0;
-                }
-            }
-        } else if(player.up) {
-            player.fall = true;
-            player.speed = -JUMP;
-            player.y -= JUMP/JUMP_DIV;
-            plat = platCollision(player);
-            if(plat) {
-                player.y = plat.y + plat.h;
-                player.speed = 1;
-            }
-        } else {
-            player.y += 1;
-            plat = platCollision(player);
-            if(plat) {
-                player.y = plat.y - PLAYER_H;
-            } else {
-                player.fall = true;
-                player.speed = 1;
-            }
-        }
-        if(player.left) {
-            player.x -= SPEED;
-            plat = platCollision(player);
-            if(plat) {
-                player.x = plat.x + plat.w;
-            }
-        }
-        if(player.right) {
-            player.x += SPEED;
-            plat = platCollision(player);
-            if(plat) {
-                player.x = plat.x - PLAYER_W;
-            }
-        }
-
-        if(player.x < 0) player.x = 0;
-        if(player.x > CANVAS_W - PLAYER_W) player.x = CANVAS_W - PLAYER_W;
-        if(player.y < 0) player.y = 0;
-        if(player.y > CANVAS_H - PLAYER_H) {
-            player.y = CANVAS_H - PLAYER_H;
-            player.fall = false;
-            player.speed = 0;
-        }
-    }
-}
-
-function playerCollision() {
     var player, test;
+    var update = {};
+
+    for(var p in players) {
+        players[p].move(platforms);
+    }
+
     for(var p in players) {
         player = players[p];
         for(var t in players) {
             test = players[t];
             if(player == test) continue;
-            if(collision(player.x, player.y, PLAYER_W, PLAYER_H, test.x, test.y, PLAYER_W, PLAYER_H)) { 
+            if(player.collide(test)) {
                 if(player.y < test.y) {
-                    setTimeout(playerReset(test), 3000);
+                    test.reset();
                 } else if(test.y < player.y) {
-                    setTimeout(playerReset(player), 3000);
+                    player.reset();
                 }
             }
         }
     }
-}
 
-function platCollision(player) {
-    var plat;
-    for(var p in platforms) {
-        plat = platforms[p];
-        if(collision(player.x, player.y, PLAYER_W, PLAYER_H, plat.x, plat.y, plat.w, plat.h)) {
-            return plat;
+    for(var p in players) {
+        player = players[p];
+        update[player.nickname] = {
+            x: player.x,
+            y: player.y,
+            w: player.w,
+            h: player.h
         }
     }
-    return false;
-}
-
-function collision(x,y,w,h,xx,yy,ww,hh) {
-    var x2 = x + w;
-    var y2 = y + h;
-    var xx2 = xx + ww;
-    var yy2 = yy + hh;
-    if (x > xx && x < xx2 && y > yy && y < yy2) {
-        return true;
-    } else if (x2 > xx && x2 < xx2 && y > yy && y < yy2) {
-        return true;
-    } else if (x > xx && x < xx2 && y2 > yy && y2 < yy2) {
-        return true;
-    } else if (x2 > xx && x2 < xx2 && y2 > yy && y2 < yy2) {
-        return true;
-    } else if (xx > x && xx < x2 && yy > y && yy < y2) {
-        return true;
-    } else if (xx2 > x && xx2 < x2 && yy > y && yy < y2) {
-        return true;
-    } else if (xx > x && xx < x2 && yy2 > y && yy2 < y2) {
-        return true;
-    } else if (xx2 > x && xx2 < x2 && yy2 > y && yy2 < y2) {
-        return true;
-    } else if (x == xx && x2 == xx2 && ((y > yy && y < yy2) || y2 > yy && y < yy2)) {
-        return true;
-    } else if (y == yy && y2 == yy2 && ((x > xx && x < xx2) || x2 > xx && x < xx2)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function playerReset(player) {
-    player.x = 0;
-    player.y = 0;
-    player.fall = true;
-    player.speed = 1;
-}
-
-function addPlatform(xx,yy,ww,hh) {
-    platforms[platforms.length] = {
-        x: xx,
-        y: yy,
-        w: ww,
-        h: hh
-    }
+    
+    io.sockets.emit('update', update);
 }
---
+++ player.js
@@ -0,0 +1,161 @@
+module.exports = function(nickname, x, y, w, h) {
+    this.nickname = nickname;
+
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+
+    this.fall = false;
+    this.speed = 0;
+
+    this.up = false;
+    this.down = false;
+    this.left = false;
+    this.right = false;
+
+    this.move = function(platforms) {
+        if(this.fall) {
+            this.speed++;
+            this.y += Math.ceil(this.speed/ACCEL_TICKS);
+            platform = this.collidePlatforms(platforms);
+            if(platform) {
+                if(this.speed <= 0) {
+                    this.y = platform.y + platform.h;
+                    this.speed = 1;
+                } else {
+                    this.y = platform.y - this.h;
+                    this.fall = false;
+                    this.speed = 0;
+                }
+            }
+        } else if(this.up) {
+            this.fall = true;
+            this.speed = -JUMP;
+            this.y -= JUMP/ACCEL_TICKS;
+            platform = this.collidePlatforms(platforms);
+            if(platform) {
+                this.y = platform.y + platform.h;
+                this.speed = 1;
+            }
+        } else {
+            this.y += 1;
+            platform = this.collidePlatforms(platforms);
+            if(platform) {
+                this.y = platform.y - this.h;
+            } else {
+                this.fall = true;
+                this.speed = 1;
+            }
+        }
+        if(this.left) {
+            this.x -= SPEED;
+            platform = this.collidePlatforms(platforms);
+            if(platform) {
+                this.x = platform.x + platform.w;
+            }
+        }
+        if(this.right) {
+            this.x += SPEED;
+            platform = this.collidePlatforms(platforms);
+            if(platform) {
+                this.x = platform.x - this.w;
+            }
+        }
+
+        if(this.x < 0) this.x = 0;
+        if(this.x > CANVAS_W - this.w) this.x = CANVAS_W - this.w;
+        if(this.y < 0) this.y = 0;
+        if(this.y > CANVAS_H - this.h) {
+            this.y = CANVAS_H - this.h;
+            this.fall = false;
+            this.speed = 0;
+        }
+    }
+
+    this.keyDown = function(key) {
+        switch(key) {
+            case 'up':
+                this.up = true;
+                break;
+            case 'down':
+                this.down = true;
+                break;
+            case 'left':
+                this.left = true;
+                break;
+            case 'right':
+                this.right = true;
+                break;
+        }
+    }
+
+    this.keyUp = function(key) {
+        switch(key) {
+            case 'up':
+                this.up = false;
+                break;
+            case 'down':
+                this.down = false;
+                break;
+            case 'left':
+                this.left = false;
+                break;
+            case 'right':
+                this.right = false;
+                break;
+        }
+    }
+
+    this.collidePlatforms = function(platforms) {
+        var platform;
+        for(var p in platforms) {
+            platform = platforms[p];
+            if(this.collide(platform)) {
+                return platform;
+            }
+        }
+        return false;
+    }
+
+    this.collide = function(obj) {
+        return this.collision(this.x, this.y, this.w, this.h, obj.x, obj.y, obj.w, obj.h);
+    }
+
+    this.collision = function(x,y,w,h,xx,yy,ww,hh) {
+        var x2 = x + w;
+        var y2 = y + h;
+        var xx2 = xx + ww;
+        var yy2 = yy + hh;
+        if (x > xx && x < xx2 && y > yy && y < yy2) {
+            return true;
+        } else if (x2 > xx && x2 < xx2 && y > yy && y < yy2) {
+            return true;
+        } else if (x > xx && x < xx2 && y2 > yy && y2 < yy2) {
+            return true;
+        } else if (x2 > xx && x2 < xx2 && y2 > yy && y2 < yy2) {
+            return true;
+        } else if (xx > x && xx < x2 && yy > y && yy < y2) {
+            return true;
+        } else if (xx2 > x && xx2 < x2 && yy > y && yy < y2) {
+            return true;
+        } else if (xx > x && xx < x2 && yy2 > y && yy2 < y2) {
+            return true;
+        } else if (xx2 > x && xx2 < x2 && yy2 > y && yy2 < y2) {
+            return true;
+        } else if (x == xx && x2 == xx2 && ((y > yy && y < yy2) || y2 > yy && y < yy2)) {
+            return true;
+        } else if (y == yy && y2 == yy2 && ((x > xx && x < xx2) || x2 > xx && x < xx2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.reset = function() {
+        this.x = 0;
+        this.y = 0;
+        this.fall = false;
+        this.speed = 0;
+    }
+}
